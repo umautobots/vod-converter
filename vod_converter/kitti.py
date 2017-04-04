4efebfaf71b5250b13df883fe248ac84f6bb09ac
krosaen <krosaen@gmail.com>
2017-04-04 09:26:21 -0400

    adds support for egesting KITTI format

--- vod_converter/kitti.py
+++ vod_converter/kitti.py
@@ -32,10 +32,10 @@ each row corresponds to one object. The 15 columns represent:
 
 import csv
 import os
+from PIL import Image
+import shutil
 
 from converter import Ingestor, Egestor
-
-# Detections
 
 
 class KITTIIngestor(Ingestor):
@@ -52,28 +52,37 @@ class KITTIIngestor(Ingestor):
         return True, None
 
     def ingest(self, path):
-        image_names = self._get_image_ids(path)
-        return [self._get_image_detection(path, image_name) for image_name in image_names]
+        image_ids = self._get_image_ids(path)
+        image_ext = 'png'
+        if len(image_ids):
+            first_image_id = image_ids[0]
+            image_ext = self.find_image_ext(path, first_image_id)
+        return [self._get_image_detection(path, image_name, image_ext=image_ext) for image_name in image_ids]
+
+    def find_image_ext(self, root, image_id):
+        for image_ext in ['png', 'jpg']:
+            if os.path.exists(f"{root}/training/image_2/{image_id}.{image_ext}"):
+                return image_ext
+        raise Exception(f"could not find jpg or png for {image_id} at {root}/training/image_2")
 
     def _get_image_ids(self, root):
         path = f"{root}/train.txt"
         with open(path) as f:
             return f.read().strip().split('\n')
 
-
-    def _get_image_detection(self, root, image_id):
+    def _get_image_detection(self, root, image_id, *, image_ext='png'):
         detections_fpath = f"{root}/training/label_2/{image_id}.txt"
         detections = self._get_detections(detections_fpath)
         detections = [det for det in detections if det['left'] < det['right'] and det['top'] < det['bottom']]
+        image_path = f"{root}/training/image_2/{image_id}.{image_ext}"
+        image_width, image_height = _image_dimensions(image_path)
         return {
             'image': {
                 'id': image_id,
-                'path': f"{root}/training/image_2/{image_id}.png",
+                'path': image_path,
                 'segmented_path': None,
-                'width': 1024,
-                'height': 512
-                # 'width': 1242,
-                # 'height': 375
+                'width': image_width,
+                'height': image_height
             },
             'detections': detections
         }
@@ -95,8 +104,59 @@ class KITTIIngestor(Ingestor):
         return detections
 
 
-# TODO
-# class KITTIEgestor(Egestor):
-#     pass
+def _image_dimensions(path):
+    with Image.open(path) as image:
+        return image.width, image.height
+
+DEFAULT_TRUNCATED = 0.0 # 0% truncated
+DEFAULT_OCCLUDED = 0    # fully visible
+
+class KITTIEgestor(Egestor):
+
+    def expected_labels(self):
+        return {
+            'Car': [],
+            'Cyclist': ['biker'],
+            'Misc': [],
+            'Pedestrian': ['person'],
+            'Person_sitting': [],
+            'Tram': [],
+            'Truck': [],
+            'Van': [],
+        }
+
+    def egest(self, *, image_detections, root):
+        images_dir = f"{root}/training/image_2"
+        os.makedirs(images_dir, exist_ok=True)
+        labels_dir = f"{root}/training/label_2"
+        os.makedirs(labels_dir, exist_ok=True)
+
+        id_file = f"{root}/train.txt"
+
+        for image_detection in image_detections:
+            image = image_detection['image']
+            image_id = image['id']
+            src_extension = image['path'].split('.')[-1]
+            shutil.copyfile(image['path'], f"{images_dir}/{image_id}.{src_extension}")
+
+            with open(id_file, 'a') as out_image_index_file:
+                out_image_index_file.write(f'{image_id}\n')
+
+            out_labels_path = f"{labels_dir}/{image_id}.txt"
+            with open(out_labels_path, 'w') as csvfile:
+                csvwriter = csv.writer(csvfile, delimiter=' ', quoting=csv.QUOTE_MINIMAL)
+
+                for detection in image_detection['detections']:
+                    kitti_row = [-1] * 12
+                    kitti_row[0] = detection['label']
+                    kitti_row[1] = DEFAULT_TRUNCATED
+                    kitti_row[2] = DEFAULT_OCCLUDED
+                    x1 = detection['left']
+                    x2 = detection['right']
+                    y1 = detection['top']
+                    y2 = detection['bottom']
+                    kitti_row[4:8] = x1, y1, x2, y2
+                    csvwriter.writerow(kitti_row)
+
 
 
--- vod_converter/main.py
+++ vod_converter/main.py
@@ -29,7 +29,7 @@ INGESTORS = {
 
 EGESTORS = {
     'voc': voc.VOCEgestor(),
-    # 'kitti': kitti.KITTIEgestor() # TODO
+    'kitti': kitti.KITTIEgestor()
 }
 
 
